7d04c0bd32a2b2f66f35c77605d5588d57703e60
elena navarro molina <elenita78.elenanavarrom@gmail.com>
2020-05-05 16:08:20 +0200

    Create component game-cover

---
+++ IceBreakersFront/src/app/components/game-cover/game-cover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-cover',
+  templateUrl: './game-cover.component.html',
+  styleUrls: ['./game-cover.component.css']
+})
+export class GameCoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
