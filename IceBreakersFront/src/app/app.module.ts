bb60441ee0e92306e538d087df47a7da10ac027f
= <=>
2020-05-13 16:33:58 +0200

    navigate to facilitation view

--- IceBreakersFront/src/app/app.module.ts
+++ IceBreakersFront/src/app/app.module.ts
@@ -1,30 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GameCoverComponent } from './components/game-cover/game-cover.component';
-import { GameDescriptionComponent } from './components/game-description/game-description.component';
-import { GameCreationComponent} from './components/game-creation/game-creation.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
 import { FormsModule} from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameCoverComponent } from './components/game-cover/game-cover.component';
+import { GameDescriptionComponent } from './components/game-description/game-description.component';
+import { GameCreationComponent} from './components/game-creation/game-creation.component';
+import { GameFacilitationComponent } from "./components/game-facilitation/game-facilitation.component";
+
 @NgModule({
   declarations: [
     AppComponent,
     GameCoverComponent,
     GameDescriptionComponent,
-    GameCreationComponent
+    GameCreationComponent,
+    GameFacilitationComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
  	  AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: GameCoverComponent },
+      { path: 'facilitation/:facilitatorUrl', component: GameFacilitationComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
