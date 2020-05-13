cd944ae362803e24e8ac3ecf0c10654ebad30e3c
= <=>
2020-05-13 11:49:49 +0200

    Create game's random urls

--- IceBreakersFront/src/app/services/game.service.ts
+++ IceBreakersFront/src/app/services/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/firestore';
 import { Game } from 'src/app/shared/models/game';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,19 +10,14 @@ export class GameService {
 
   gamesRef: AngularFirestoreCollection<Game>;
   game:Game;
+  
   constructor(private firestore: AngularFirestore) { 
     this.gamesRef = this.firestore.collection<Game>('games');
   }
 
   createGame(gameName:string){
-    
-     this.game = {
-      name : gameName, 
-      facilitatorUrl: "jfdsh",
-      participantUrl: "asfoi",     
-     }     
-    
-    return this.gamesRef.add(this.game);  
-
-  }
+    this.game = new Game(gameName);
+        
+    return this.gamesRef.add({...this.game});
+  }  
 }
--- IceBreakersFront/src/app/shared/models/game.ts
+++ IceBreakersFront/src/app/shared/models/game.ts
@@ -1,9 +1,17 @@
+import { generate } from "shortid";
+
 export class Game {
     //id: string;
     name: string;
     facilitatorUrl: string;
     participantUrl: string;
 
+    constructor(name:string){
+        this.name = name;
+        this.facilitatorUrl= generate();
+        this.participantUrl= generate();  
+    } 
+    
     // deserialize(input: any) {
     //     Object.assign(this, input);
     //     return this;
